98cdcd1083b87a723f019c6b3437a81ee4d34568
Hemang Patel <hemang9705@gmail.com>
2025-12-06 15:55:41 +0530

    payment schema

--- src/types/payment.ts
+++ src/types/payment.ts
@@ -1,10 +1,16 @@
 export interface Payment {
-  paymentId: number;
-  amount: number;
-  paymentMode: string;
+  paymentId?: number
   paymentType: string;
+  enrollmentId?: number | null;
+  paymentMode: string;
   transactionId?: string | null;
+  totalAmount: number;
+  paid: number;
+  remaining: number;
   paymentRemarks?: string | null;
-  enrollmentId: number;
-  createdAt: Date;
+  createdAt?: string | Date;
+
+  academyName?: string | null;
+  courseName?: string | null;
+  memberName?: string | null;
 }
