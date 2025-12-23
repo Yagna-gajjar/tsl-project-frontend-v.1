58ead08b580b778b4c1ca22efa8199aece0f1908
Hemang Patel <hemang9705@gmail.com>
2025-12-23 09:36:22 +0530

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path';
-import eslint from 'vite-plugin-eslint'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint({
-    failOnError: false,
-    failOnWarning: false
-  })],
+  plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
