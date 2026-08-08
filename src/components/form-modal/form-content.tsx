9e0e4db52c844d11998c3c6ee7d85d537d8c7e77
Hemang Patel <hemang9705@gmail.com>
2026-08-08 13:06:08 +0530

    feat(transaction): add enrollment search and load more functionality in transaction form modal

--- src/components/form-modal/form-content.tsx
+++ src/components/form-modal/form-content.tsx
@@ -103,6 +103,7 @@ export function FormContent<T extends Record<string, any>>({
             maxDate={field.maxDate}
             isLoadingMore={field.isLoadingMore}
             onLoadMore={field.onLoadMore}
+            onSearch={field.onSearch}
             hidden={field.hidden}
           />
         ))}
